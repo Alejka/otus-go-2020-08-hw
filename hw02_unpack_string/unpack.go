153475efed734e5a8c8dc949c7e018544d682a2f
Oleksii Samokhvalov <samohvalov.aleksey@gmail.com>
2020-09-20 21:59:32 +0300

    fix Travis CI checks: golang-cli

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -9,98 +9,91 @@ import (
 	valid "github.com/asaskevich/govalidator"
 )
 
+// ErrInvalidString shoud be use for testing.
 var ErrInvalidString = errors.New("invalid string")
 
-// Unpack unpacks the string according to the format string
+// Unpack unpacks the string according to the format string.
 func Unpack(str string) (string, error) {
-	var err error
-
 	if !utf8.ValidString(str) {
-		err = ErrInvalidString
-		return "", err
+		return "", ErrInvalidString
 	}
 
-	runesCount := utf8.RuneCountInString(str)
-	if runesCount == 0 {
-		return "", err
+	if utf8.RuneCountInString(str) == 0 {
+		return "", nil
 	}
 
+	return unpackValidString(str)
+}
+
+func unpackValidString(str string) (string, error) {
+	var err error
 	var unpackedStrBuilder strings.Builder
-
-	var repeatCountStrBuilder strings.Builder
-	repeatRune := rune(-1)
-	repeatCount := 1
-
+	var repeatCountStrBuilder strings.Builder // stores string with count of char repeat (ex. 15 and 7 for string "a15b7f")
+	runesCount := utf8.RuneCountInString(str)
+	strRune := ""            // the rune in string type
+	strRepeatedRune := ""    // the repeated rune in string type
+	runeToRepeat := rune(-1) // the rune which should repeat
 	escapedRune := '\\'
 	isEscapeLastRune := false
 
 	runeIndex := 0
-	strRune := ""
 	for _, rune := range str {
 		runeIndex++
 		strRune = string(rune)
 
-		// first rune cannot be digit
-		if runeIndex == 1 && valid.IsInt(strRune) {
-			err = ErrInvalidString
-			return "", err
+		if runeIndex == 1 && valid.IsInt(strRune) { // first rune cannot be digit
+			return "", ErrInvalidString
 		}
-		// if escaped char locates before letter
-		if isEscapeLastRune && !valid.IsInt(strRune) && rune != escapedRune {
-			err = ErrInvalidString
-			return "", err
+
+		if isEscapeLastRune && !valid.IsInt(strRune) && rune != escapedRune { // if escaped char locates before letter
+			return "", ErrInvalidString
 		}
 
 		if valid.IsInt(strRune) && !isEscapeLastRune { // if rune is digit (not escape)
 			repeatCountStrBuilder.WriteRune(rune)
 			continue
-		} else {
-			// if rune is "escape" character
-			if rune == escapedRune && !isEscapeLastRune {
-				// last rune cannot be `\`
-				if runeIndex == runesCount {
-					err = ErrInvalidString
-					return "", err
-				}
-
-				isEscapeLastRune = true
-			} else {
-				// if rune is char: repeat previous rune
-				if repeatRune != -1 {
-					if repeatCountStrBuilder.Len() > 0 {
-						repeatCount, err = strconv.Atoi(repeatCountStrBuilder.String())
-						if err != nil {
-							return "", err
-						}
-					} else {
-						repeatCount = 1
-					}
-					unpackedStrBuilder.WriteString(strings.Repeat(string(repeatRune), repeatCount))
-				}
-
-				repeatRune = rune
-				repeatCountStrBuilder.Reset()
-				isEscapeLastRune = false
-			}
 		}
-	}
 
-	// process last rune
-	if repeatRune != -1 {
-		if repeatCountStrBuilder.Len() > 0 {
-			repeatCount, err = strconv.Atoi(repeatCountStrBuilder.String())
+		if rune == escapedRune && !isEscapeLastRune { // if rune is "escape" character
+			if runeIndex == runesCount { // last rune cannot be `\`
+				return "", ErrInvalidString
+			}
+			isEscapeLastRune = true
+			continue
+		}
+
+		if runeToRepeat != -1 { // if rune is char: repeat previous rune
+			strRepeatedRune, err = repeatRune(runeToRepeat, repeatCountStrBuilder)
 			if err != nil {
 				return "", err
 			}
-		} else {
-			repeatCount = 1
+			unpackedStrBuilder.WriteString(strRepeatedRune)
 		}
-		unpackedStrBuilder.WriteString(strings.Repeat(string(repeatRune), repeatCount))
+
+		runeToRepeat = rune
+		repeatCountStrBuilder.Reset()
+		isEscapeLastRune = false
 	}
 
-	if err != nil {
-		unpackedStrBuilder.Reset()
+	if runeToRepeat != -1 { // process last rune
+		strRepeatedRune, err = repeatRune(runeToRepeat, repeatCountStrBuilder)
+		if err != nil {
+			return "", err
+		}
+		unpackedStrBuilder.WriteString(strRepeatedRune)
 	}
 
 	return unpackedStrBuilder.String(), err
+}
+
+func repeatRune(runeToRepeat rune, repeatCountStrBuilder strings.Builder) (string, error) {
+	if repeatCountStrBuilder.Len() == 0 {
+		return string(runeToRepeat), nil
+	}
+
+	repeatCount, err := strconv.Atoi(repeatCountStrBuilder.String())
+	if err != nil {
+		return "", err
+	}
+	return strings.Repeat(string(runeToRepeat), repeatCount), nil
 }
