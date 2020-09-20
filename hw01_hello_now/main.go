b71fa167c9edf83ab8a54da9f274b421541e112c
Oleksii Samokhvalov <samohvalov.aleksey@gmail.com>
2020-09-20 16:10:23 +0300

    transfer currTime after ntpTime

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -9,11 +9,11 @@ import (
 )
 
 func main() {
-	currTime := time.Now()
 	ntpTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
 	if err != nil {
 		log.Fatal("Error:\n", err)
 	}
+	currTime := time.Now()
 
 	fmt.Printf("current time: %v\nexact time: %v\n", currTime.Round(0), ntpTime.Round(0))
 }
